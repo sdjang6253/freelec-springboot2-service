ccd6a4f75a891be9699548e02985c61cc4f9dea6
sdjang <qkswl6253@naver.com>
2021-04-04 01:44:56 +0900

    교재 P.95 까지 진행.
    해볼것.
    ORM 과 SQL Mapper 사이의 장단점 확인해보기
    Redis 가 무엇인지 찾아보기.

--- src/main/java/com/sdjang/book/springboot/web/HelloController.java
+++ src/main/java/com/sdjang/book/springboot/web/HelloController.java
@@ -1,6 +1,9 @@
 package com.sdjang.book.springboot.web;
 
+import com.sdjang.book.springboot.web.dto.HelloResponseDto;
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,10 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name , @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
