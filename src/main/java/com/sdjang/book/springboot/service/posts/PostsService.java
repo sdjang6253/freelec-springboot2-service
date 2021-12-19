5682319fc1703802749bd6063e89f65703ef7b57
sdjang6253 <qkswl625341@gmail.com>
2021-12-19 23:37:35 +0900

    p.150 까지 실습 ( 생성된 포스트 리스트 확인 까지) 확인해볼 이슈 = @Transactional(readOnly = true) 는 왜 주는건지??(그냥 안주면 되는거 아닌지?)

--- src/main/java/com/sdjang/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sdjang/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.sdjang.book.springboot.service.posts;
 
 import com.sdjang.book.springboot.domain.posts.Posts;
 import com.sdjang.book.springboot.domain.posts.PostsRepository;
+import com.sdjang.book.springboot.web.dto.PostsListResponseDto;
 import com.sdjang.book.springboot.web.dto.PostsResponseDto;
 import com.sdjang.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sdjang.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,7 +19,7 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
@@ -24,13 +27,19 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
 
-        posts.update(requestDto.getTitle() , requestDto.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public Object findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/sdjang/book/springboot/web/PostsApiController.java
+++ src/main/java/com/sdjang/book/springboot/web/PostsApiController.java
@@ -14,14 +14,14 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id , @RequestBody PostsUpdateRequestDto requestDto) {
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
 
-        return postsService.update(id , requestDto);
+        return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
