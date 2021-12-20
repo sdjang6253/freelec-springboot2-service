a473efef27c7ba9f320b2db8c76077614679a000
sdjang6253 <qkswl625341@gmail.com>
2021-12-20 23:55:06 +0900

    P.162까지 개발

--- src/main/java/com/sdjang/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sdjang/book/springboot/service/posts/PostsService.java
@@ -41,5 +41,11 @@ public class PostsService {
     public Object findAllDesc() {
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
 
 }
